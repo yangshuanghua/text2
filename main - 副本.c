64dffa1a795923db1564ba83bd95ec66325a8b01
yangshuanghua <40491757+yangshuanghua@users.noreply.github.com>
2018-07-02 11:55:08 +0800

    Revert "text offline"
    This reverts commit 30efb53a86a9f8044c10deab0eb7c1a0238e3baf.

--- main - 副本.c
+++ main - 副本.c
@@ -5,6 +5,7 @@ while(1)
 ;//hahah 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 //fuck 
 if (i==0)
 {
@@ -17,6 +18,10 @@ i++;
 
 }
 >>>>>>> parent of 926a1f8... delet main
+=======
+
+}
+>>>>>>> parent of 30efb53... text offline
 =======
 
 }
